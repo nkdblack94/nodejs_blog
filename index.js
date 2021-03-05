49358788d3aede9bf7d1ae2a321f506cf65dd96b
nkdblack94 <nkdblack1294@gmail.com>
2021-03-05 20:50:42 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/home', (req, res) => res.send("hello world! 1234"));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
